6b2df33b9794a03771f395753ede13b8c8647713
sulaimangm <sulaimangm@gmail.com>
2023-11-10 17:53:21 -0500

    Updating Product Controller

---
+++ ContosoCrafts.Website/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using ContosoCrafts.Website.Models;
+using ContosoCrafts.Website.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoCrafts.Website.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+    }
+}
